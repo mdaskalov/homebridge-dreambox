b12d04e04147cbdc019081c9368d0ed3866783e4
Milko Daskalov <milko.daskalov@mpay24.com>
2020-02-19 16:39:06 +0100

    use modern js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const DreamboxPlatform = require('./dreambox-platform').DreamboxPlatform;
+const DreamboxPlatform = require('./dreambox-platform');
 
-module.exports = function (homebridge) {
+module.exports = homebridge => {
   // For platform plugin to be considered as dynamic platform plugin,
   // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
   homebridge.registerPlatform(
