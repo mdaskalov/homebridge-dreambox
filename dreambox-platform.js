37ead51108bda69e669f9fbccf3cb6b5ccebaebb
Milko Daskalov <milko.daskalov@mpay24.com>
2020-02-19 16:40:14 +0100

    created separate device file

--- dreambox-platform.js
+++ dreambox-platform.js
@@ -1,71 +1,37 @@
-var http = require('http');
-var Accessory, Service, Characteristic, UUIDGen;
+const DreamboxDevice = require('./dreambox-device');
 
-module.exports = {
-  DreamboxPlatform: DreamboxPlatform
-};
+class DreamboxPlatform {
+  // Platform constructor
+  // config may be null
+  // api may be null if launched from old homebridge version
+  constructor(log, config, api) {
+    this.log = log;
+    this.config = config;
+    this.name = config["name"];
 
-// Platform constructor
-// config may be null
-// api may be null if launched from old homebridge version
-function DreamboxPlatform(log, config, api) {
-  var platform = this;
-  this.log = log;
-  this.config = config;
-  this.accessories = [];
+    this.log("DreamboxPlatform Init");
 
-  log("DreamboxPlatform Init");
-
-  this.requestServer = http.createServer(function (request, response) {
-    if (request.url === "/add") {
-      this.addAccessory(new Date().toISOString());
-      response.writeHead(204);
-      response.end();
+    if (this.version < 2.1) {
+      throw new Error('Unexpected API version.');
     }
 
-    if (request.url == "/reachability") {
-      this.updateAccessoriesReachability();
-      response.writeHead(204);
-      response.end();
+    if (api) {
+      // Save the API object as plugin needs to register new accessory via this object
+      this.api = api;
+
+      this.accessory = new DreamboxDevice(log, this.config, this.api);
+
+      // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories.
+      // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
+      // Or start discover new accessories.
+      this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
     }
-
-    if (request.url == "/remove") {
-      this.removeAccessory();
-      response.writeHead(204);
-      response.end();
-    }
-  }.bind(this));
-
-  this.requestServer.listen(18081, function () {
-    platform.log("Server Listening...");
-  });
-
-  if (api) {
-    // Save the API object as plugin needs to register new accessory via this object
-    this.api = api;
-
-    // Accessory must be created from PlatformAccessory Constructor
-    Accessory = this.api.platformAccessory;
-
-    // Service and Characteristic are from hap-nodejs
-    Service = this.api.hap.Service;
-    Characteristic = this.api.hap.Characteristic;
-    UUIDGen = this.api.hap.uuid;
-
-    // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories.
-    // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-    // Or start discover new accessories.
-    this.api.on('didFinishLaunching', function () {
-      platform.log("DidFinishLaunching");
-    }.bind(this));
   }
-}
 
-DreamboxPlatform.prototype = {
   // Function invoked when homebridge tries to restore cached accessory.
   // Developer can configure accessory at here (like setup event handler).
   // Update current value.
-  configureAccessory: function (accessory) {
+  configureAccessory(accessory) {
     this.log(accessory.displayName, "Configure Accessory");
     var platform = this;
 
@@ -74,149 +40,15 @@ DreamboxPlatform.prototype = {
     // accessory.updateReachability()
     accessory.reachable = true;
 
-    accessory.on('identify', function (paired, callback) {
+    accessory.on('identify', (paired, callback) => {
       platform.log(accessory.displayName, "Identify!!!");
       callback();
     });
-
-    if (accessory.getService(Service.Lightbulb)) {
-      accessory.getService(Service.Lightbulb)
-        .getCharacteristic(Characteristic.On)
-        .on('set', function (value, callback) {
-          platform.log(accessory.displayName, "Light -> " + value);
-          callback();
-        });
-    }
-
-    this.accessories.push(accessory);
-  },
-
-  // Handler will be invoked when user try to config your plugin.
-  // Callback can be cached and invoke when necessary.
-  configurationRequestHandler: function (context, request, callback) {
-    this.log("Context: ", JSON.stringify(context));
-    this.log("Request: ", JSON.stringify(request));
-
-    // Check the request response
-    if (request && request.response && request.response.inputs && request.response.inputs.name) {
-      this.addAccessory(request.response.inputs.name);
-
-      // Invoke callback with config will let homebridge save the new config into config.json
-      // Callback = function(response, type, replace, config)
-      // set "type" to platform if the plugin is trying to modify platforms section
-      // set "replace" to true will let homebridge replace existing config in config.json
-      // "config" is the data platform trying to save
-      callback(null, "platform", true, {
-        "platform": "Dreambox",
-        "otherConfig": "SomeData"
-      });
-      return;
-    }
-
-    // - UI Type: Input
-    // Can be used to request input from user
-    // User response can be retrieved from request.response.inputs next time
-    // when configurationRequestHandler being invoked
-
-    var respDict = {
-      "type": "Interface",
-      "interface": "input",
-      "title": "Add Accessory",
-      "items": [{
-          "id": "name",
-          "title": "Name",
-          "placeholder": "Fancy Light"
-        } //,
-        // {
-        //   "id": "pw",
-        //   "title": "Password",
-        //   "secure": true
-        // }
-      ]
-    }
-
-    // - UI Type: List
-    // Can be used to ask user to select something from the list
-    // User response can be retrieved from request.response.selections next time
-    // when configurationRequestHandler being invoked
-
-    // var respDict = {
-    //   "type": "Interface",
-    //   "interface": "list",
-    //   "title": "Select Something",
-    //   "allowMultipleSelection": true,
-    //   "items": [
-    //     "A","B","C"
-    //   ]
-    // }
-
-    // - UI Type: Instruction
-    // Can be used to ask user to do something (other than text input)
-    // Hero image is base64 encoded image data. Not really sure the maximum length HomeKit allows.
-
-    // var respDict = {
-    //   "type": "Interface",
-    //   "interface": "instruction",
-    //   "title": "Almost There",
-    //   "detail": "Please press the button on the bridge to finish the setup.",
-    //   "heroImage": "base64 image data",
-    //   "showActivityIndicator": true,
-    // "showNextButton": true,
-    // "buttonText": "Login in browser",
-    // "actionURL": "https://google.com"
-    // }
-
-    // Plugin can set context to allow it track setup process
-    context.ts = "Hello";
-
-    // Invoke callback to update setup UI
-    callback(respDict);
-  },
-
-  // Sample function to show how developer can add accessory dynamically from outside event
-  addAccessory: function (accessoryName) {
-    this.log("Add Accessory");
-    var platform = this;
-    var uuid;
-
-    uuid = UUIDGen.generate(accessoryName);
-
-    var newAccessory = new Accessory(accessoryName, uuid);
-    newAccessory.on('identify', function (paired, callback) {
-      platform.log(newAccessory.displayName, "Identify!!!");
-      callback();
-    });
-
-    //newAccessory.category = Accessory.Categories.TELEVISION;
-
-    // Plugin can save context on accessory to help restore accessory in configureAccessory()
-    // newAccessory.context.something = "Something"
-
-    // Make sure you provided a name for service, otherwise it may not visible in some HomeKit apps
-    newAccessory.addService(Service.Lightbulb, "Dreambox")
-      .getCharacteristic(Characteristic.On)
-      .on('set', function (value, callback) {
-        platform.log(newAccessory.displayName, "Dreambox -> " + value);
-        callback();
-      });
-
-    this.accessories.push(newAccessory);
-    this.api.registerPlatformAccessories("homebridge-dreambox", "Dreambox", [newAccessory]);
-  },
-
-  updateAccessoriesReachability: function () {
-    this.log("Update Reachability");
-    for (var index in this.accessories) {
-      var accessory = this.accessories[index];
-      accessory.updateReachability(false);
-    }
-  },
-
-  // Sample function to show how developer can remove accessory dynamically from outside event
-  removeAccessory: function () {
-    this.log("Remove Accessory");
-    this.api.unregisterPlatformAccessories("homebridge-dreambox", "Dreambox", this.accessories);
-
-    this.accessories = [];
   }
-};
+
+  didFinishLaunching() {
+    this.log.debug('didFinishLaunching');
+  }
+}
+
+module.exports = DreamboxPlatform;
