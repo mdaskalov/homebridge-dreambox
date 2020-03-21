baf735673b20f538ca2c3b480a4956667137c421
Milko Daskalov <mdaskalov@outlook.com>
2020-03-21 14:56:45 +0100

    configurable broker host and auth parameters

--- mqtt-client.js
+++ mqtt-client.js
@@ -3,62 +3,44 @@ const mqtt = require('mqtt');
 class MQTTClient {
     constructor(log, config) {
         this.log = log;
-        this.config = config;
-
-        this.config.name = 'dream';
-        this.config.url = 'mqtt://raspi2:1883';
-
         this.mqttDispatch = [];
 
-        this.log('MQTT Client created');
-
-        this.mqttClient = this.mqttInit();
-    }
-
-    mqttInit() {
-        var clientId = 'dreambox_' + this.config.name.replace(/[^\x20-\x7F]/g, '') + '_' + Math.random().toString(16).substr(2, 8);
-
         var options = {
-            keepalive: 10,
-            clientId: clientId,
+            clientId: 'homebridge-dreambox_' + Math.random().toString(16).substr(2, 8),
             protocolId: 'MQTT',
             protocolVersion: 4,
             clean: true,
-            reconnectPeriod: 1000,
-            connectTimeout: 30 * 1000,
-            will: {
-                topic: 'WillMsg',
-                payload: 'Connection Closed abnormally..!',
-                qos: 0,
-                retain: false
-            },
-            // username: this.config.username,
-            // password: this.config.password,
-            rejectUnauthorized: false
+            reconnectPeriod: 30000,
+            connectTimeout: 30000,
+            username: config.mqttUsername,
+            password: config.mqttPassword
         };
 
-        var mqttClient = mqtt.connect(this.config.url, options);
-        mqttClient.on('error', function (err) {
-            this.log('MQTT Error: ' + err);
+        let broker = config.mqttBroker || 'localhost';
+        this.mqttClient = mqtt.connect('mqtt://' + broker, options);
+        this.mqttClient.on('error', err => {
+            this.log.error('MQTT Error: %s', err.message);
         });
 
-        mqttClient.on('message', (topic, message) => {
-            this.log.debug('Received MQTT: ' + topic + ' = ' + message);
+        this.mqttClient.on('message', (topic, message) => {
+            this.log.debug('MQTT Received: %s :- %s', topic, message);
             var handlers = this.mqttDispatch[topic];
             if (handlers) {
                 handlers.forEach(handler => handler(topic, message));
             } else {
-                this.log('Warning: No MQTT dispatch handler for topic [' + topic + ']');
+                this.log.warn('Warning: No MQTT dispatch handler for topic [' + topic + ']');
             }
         });
 
-        return mqttClient;
+        this.log('MQTT Client initialized');
     }
 
     mqttSubscribe(topic, handler) {
-        this.log.debug('MQTT subscribed ' + topic);
-        this.mqttDispatch[topic] = [handler];
-        this.mqttClient.subscribe(topic);
+        if (this.mqttClient) {
+            this.log.debug('MQTT Subscribed: %s', topic);
+            this.mqttDispatch[topic] = [handler];
+            this.mqttClient.subscribe(topic);
+        }
     }
 }
 
