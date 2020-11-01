ce4e5e669c2335384e7f1830abc520e6ca8494fe
Milko Daskalov <mdaskalov@outlook.com>
2020-11-01 21:49:59 +0100

    added dreambox class

---
+++ dreambox.js
@@ -0,0 +1,105 @@
+const fetch = require('node-fetch');
+const xml2js = require('xml2js');
+const url = require('url');
+
+class Dreambox {
+  constructor(platform, device) {
+    this.platform = platform;
+    this.log = platform.log;
+
+    this.powerState = false;
+    this.muteState = false;
+    this.volumeState = 0;
+    this.channel = 0;
+    this.channels = [];
+
+    this.uuid = platform.deviceUUID(device);
+    this.name = device['name'];
+    this.hostname = device['hostname'];
+    this.port = device['port'];
+    this.username = device['username'];
+    this.password = device['password'];
+    this.bouquet = device['bouquet'] || 'Favourites (TV)';
+
+    this.getDeviceInfo();
+
+    // Setup MQTT subscriptions
+    var topic = device['mqttTopic'];
+    if (platform.mqttClient && topic) {
+      platform.mqttClient.mqttSubscribe(topic + '/state/power', (topic, message) => {
+        let msg = JSON.parse(message);
+        this.powerState = (msg.power === 'True');
+        this.log.debug('Device: %s, MQTT Power: %s', this.hostname, this.getPowerStateString());
+      });
+      platform.mqttClient.mqttSubscribe(topic + '/state/channel', (topic, message) => {
+        let msg = JSON.parse(message);
+        this.channels.find((channel, index) => {
+          if (channel.name === msg.name) {
+            this.channel = index;
+            this.log.debug('Device: %s, MQTT Channel: %s :- %s (%s)', this.hostname, this.channel, channel.name, channel.reference);
+            return true;
+          }
+        });
+      });
+    }
+    
+  }
+
+  getMuteString() {
+    return this.muteState ? 'ON' : 'OFF';
+  }
+
+  getPowerStateString() {
+    return this.powerState ? 'ON' : 'STANDBY';
+  }
+
+  callEnigmaWebAPI(path, query, options = {}) {
+    return new Promise((resolve, reject) => {
+      const endpoint = url.format({
+        protocol: 'http',
+        hostname: this.hostname,
+        port: this.port,
+        pathname: '/web/' + path,
+        query: query
+      });
+
+      if (this.username && this.password) {
+        let auth = Buffer.from(encodeURIComponent(this.username) + ':' + encodeURIComponent(this.password));
+        var headers = {
+          'Authorization': 'Basic ' + auth.toString('base64')
+        };
+        options.headers = headers;
+      }
+
+      fetch(endpoint, options)
+        .then(res => res.text())
+        .then(body => xml2js.parseStringPromise(body, {
+          trim: true,
+          explicitArray: false
+        }))
+        .then(res => resolve(res))
+        .catch(err => {
+          this.log.error('Device: %s, API Call: %s, Error: ', this.hostname, endpoint, err.message);
+          reject(err);
+        });
+    });
+  }
+
+  getDeviceInfo() {
+    this.log.debug('Device: %s, getDeviceInfo', this.hostname);
+    this.callEnigmaWebAPI('about')
+      .then(res => {
+        if (res && res.e2abouts && res.e2abouts.e2about) {
+          // Device Info
+          this.manufacturer = 'Dream Multimedia';
+          this.modelName = res.e2abouts.e2about.e2model;
+          this.serialNumber = res.e2abouts.e2about.e2lanmac;
+          this.firmwareRevision = res.e2abouts.e2about.e2enigmaversion;
+        }
+      })
+      .catch(err => this.log(err));
+  }
+
+}
+
+module.exports = Dreambox;
