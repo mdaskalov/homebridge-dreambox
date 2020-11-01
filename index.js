f5a2f0b83c792d573f50778539f89b70a4fe35d6
Milko Daskalov <mdaskalov@outlook.com>
2020-11-01 20:35:33 +0100

    use plugin constants

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const DreamboxPlatform = require('./dreambox-platform');
+const {
+  PLUGIN_NAME,
+  PLATFORM_NAME,
+  DreamboxPlatform
+} = require('./dreambox-platform');
 
 module.exports = homebridge => {
   // For platform plugin to be considered as dynamic platform plugin,
   // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
   homebridge.registerPlatform(
-    'homebridge-dreambox',
-    'Dreambox',
+    PLUGIN_NAME,
+    PLATFORM_NAME,
     DreamboxPlatform,
     true
   );
