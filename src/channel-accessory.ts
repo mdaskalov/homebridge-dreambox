c6ce9018a138c17a5845e9758f848e121cceb0f6
Milko Daskalov <mdaskalov@outlook.com>
2025-06-22 13:53:46 +0200

    improved inizialization

--- src/channel-accessory.ts
+++ src/channel-accessory.ts
@@ -2,25 +2,34 @@ import { LogLevel, PlatformAccessory, Service } from 'homebridge';
 import { DreamboxPlatform } from './platform';
 import { Dreambox } from './dreambox';
 
+export type DreamboxDeviceChannel = {
+  name: string,
+  ref: string,
+}
+
 export class ChannelAccessory {
   protected service: Service;
   private name: string;
-  private reference: string;
+  private ref: string;
 
-  constructor(protected readonly platform: DreamboxPlatform, protected readonly accessory: PlatformAccessory, private readonly dreambox: Dreambox) {
-    this.name = accessory.context.channel.name;
-    this.reference = accessory.context.channel.ref;
+  constructor(
+    protected readonly platform: DreamboxPlatform,
+    protected readonly accessory: PlatformAccessory,
+    private readonly dreambox: Dreambox) {
+
+    this.name = accessory.displayName;
+    this.ref = accessory.context.ref;
 
     this.service = accessory.getService(platform.Service.Switch) || accessory.addService(platform.Service.Switch);
-    this.service.setCharacteristic(platform.Characteristic.Name, accessory.context.channel.name);
+    this.service.setCharacteristic(platform.Characteristic.Name, this.name);
 
     this.service.getCharacteristic(platform.Characteristic.On)
       .onGet(() => 0)
       .onSet(async value => {
-        this.dreambox.log(LogLevel.DEBUG, 'ChannelAccessory: setChannel: %s (%s)', this.name, this.reference);
+        this.dreambox.log(LogLevel.DEBUG, 'ChannelAccessory: setChannel: %s (%s)', this.name, this.ref);
         this.service.getCharacteristic(this.platform.Characteristic.On).updateValue(value);
         try {
-          await this.dreambox.setChannelByRef(this.reference);
+          await this.dreambox.setChannelByRef(this.ref);
         } catch (err) {
           this.dreambox.logError('ChannelAccessory: setChannel', err);
           throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
